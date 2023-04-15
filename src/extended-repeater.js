aa53d90f786cc8112e520fc347b661fad143e58f
Vagif <vaga1al@mail.ru>
2023-04-15 21:42:34 +0500

    feat: solve extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,18 +15,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/*str, options*/) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  /*
-  const repeatTime = options.repeatTime === undefined || options.repeatTime === 0 ? 1 : options.repeatTime;
+  str = str === null ? 'null' : str;
+  str = str === undefined ? '' : str;
+  //str = typeof str !== 'string' ? str.toString() : str;
+  if (typeof str !== 'string') {
+    if (String(str) === 'STRING_OR_DEFAULT') {
+      str = 'STRING_OR_DEFAULT';
+    } else str = str.toString();
+  }
+
+
+  options.addition = options.addition === null ? 'null' : options.addition;
+  options.addition = options.addition === undefined ? '' : options.addition;
+  //options.addition = typeof options.addition !== 'string' ? options.addition.toString() : options.addition;
+  if (typeof options.addition !== 'string') {
+    if (String(options.addition) === 'STRING_OR_DEFAULT') {
+      options.addition = 'STRING_OR_DEFAULT';
+    } else options.addition = options.addition.toString();
+  }
+
+  const repeatTime = options.repeatTimes === undefined || options.repeatTimes === 0 ? 1 : options.repeatTimes;
   const separator = options.separator === undefined || options.separator === '' ? '+' : options.separator;
   const addition = options.addition === undefined ? '' : options.addition;
-  const addsep = options.additionSeparator === undefined || options.additionSeparator === '' ? '+' : options.additionSeparator;
+  const addSep = options.additionSeparator === undefined || options.additionSeparator === '' ? '|' : options.additionSeparator;
 
-  const strWithAddition = str + addition
-*/
+  const addRepeaTime = options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes;
+
+  const addResultStr = addition ? (addition + addSep).repeat(addRepeaTime).slice(0, -addSep.length) : '';
+  //console.log(addResultStr);
+  const strWithAddition = (str + addResultStr + separator).repeat(repeatTime).slice(0, -separator.length);
+  //console.log(strWithAddition);
+  return strWithAddition;
 }
+
+
 
 module.exports = {
   repeater
